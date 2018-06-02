3103a7518cc173e73ab32b8dc0e89e4c740d92f2
vagrant <vagrant@localhost.localdomain>
2018-06-02 09:07:07 +0000

    quiet download

--- java.sh
+++ java.sh
@@ -6,7 +6,7 @@ if ! [ -L /usr/bin/javac ]; then
 yum update -y
  
 cd /tmp
-wget --no-cookies --no-check-certificate --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" "http://download.oracle.com/otn-pub/java/jdk/8u171-b11/512cd62ec5174c3487ac17c61aaa89e8/jdk-8u171-linux-i586.rpm"
+wget -q --no-cookies --no-check-certificate --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" "http://download.oracle.com/otn-pub/java/jdk/8u171-b11/512cd62ec5174c3487ac17c61aaa89e8/jdk-8u171-linux-i586.rpm"
  
 sudo yum install -y jdk-8u171-linux-i586.rpm
  
