36f64a7c3db29961c751f991bd9d30ceb155de2e
vagrant <vagrant@localhost.localdomain>
2018-06-02 09:04:20 +0000

    java

--- java.sh
+++ java.sh
@@ -6,10 +6,10 @@ if ! [ -L /usr/bin/javac ]; then
 yum update -y
  
 cd /tmp
-wget --no-cookies --no-check-certificate --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" "http://download.oracle.com/otn-pub/java/jdk/8u60-b27/jdk-8u60-linux-x64.rpm"
+wget --no-cookies --no-check-certificate --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" "http://download.oracle.com/otn-pub/java/jdk/8u171-b11/512cd62ec5174c3487ac17c61aaa89e8/jdk-8u171-linux-i586.rpm"
  
-sudo yum local install -y jdk-8u60-linux-x64.rpm
+sudo yum install -y jdk-8u171-linux-i586.rpm
  
-rm jdk-8u60-linux-x64.rpm
+rm jdk-8u171-linux-i586.rpm
 
 fi
