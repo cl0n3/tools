06598c44a4a352dc0d1d78b0d9211060e74cad8a
Rohan <alpha@cl0n3.net>
2018-06-02 23:38:47 +0000

    fixed perms for db, added git config

---
+++ git.sh
@@ -0,0 +1,4 @@
+
+
+git config --global user.name "Rohan"
+git config --global user.email alpha@cl0n3.net
--- postgres.sh
+++ postgres.sh
@@ -15,7 +15,7 @@ PG_HBA="/var/lib/pgsql/data/pg_hba.conf"
 sed -i "s/#listen_addresses = 'localhost'/listen_addresses = '*'/" "$PG_CONF"
 
 # Append to pg_hba.conf to add password auth:
-echo "host    all             all             all                     md5" >> "$PG_HBA"
+sed -i "s/ident/md5/" "$PG_CONF"
 
 # Explicitly set default client_encoding
 echo "client_encoding = utf8" >> "$PG_CONF"
